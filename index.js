bbf846f5a052e7ce07180f4c895f54d04f7b56ed
abdimal <abdimalik14586@gmail.com>
2024-01-22 17:44:46 +0300

    interactive quiz game

---
+++ index.js
@@ -0,0 +1,26 @@
+function result(){
+    var score = 0;
+    if(document.getElementById('correct1').checked){
+        score++;
+    }
+    if(document.getElementById('correct2').checked){
+        score++;
+    }
+    if(document.getElementById('correct3').checked){
+        score++;
+    }
+    if(document.getElementById('correct4').checked){
+        score++;
+    }
+    if(document.getElementById('correct5').checked){
+        score++;
+    }
+    if(document.getElementById('correct6').checked){
+        score++;
+    }
+    if(document.getElementById('correct7').checked){
+        score++;
+    }
+    // alert("your score is:" + score)
+    document.write("your score is " + score);
+}
